2e4a2ca7d484d0bb90bea7bec5c8687a9d7a5029
Abdallah-Issa Kass <issakass02@gmail.com>
2023-12-04 23:34:57 +0200

    bug fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,7 +20,7 @@ function Hero() {
 						Flask. Reach out if you'd like to learn more!
 					</p>
 					<DownloadButton
-						fileToDownload={"/cv.pdf"}
+						fileToDownload={`${import.meta.env.VITE_BASE_URL}cv.pdf`}
 						filename={"issakass_cv.pdf"}
 						text={"Download my CV"}
 						greetingText={"Thanks 👍"}
