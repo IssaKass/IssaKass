1df1e14e3b312960d9dd713165ccd2cf860595c3
Abdallah-Issa Kass <issakass02@gmail.com>
2023-12-05 12:44:15 +0200

    pwa integration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,56 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression2";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/IssaKass/",
-	plugins: [react(), compression()],
+	plugins: [
+		react(),
+		compression(),
+		VitePWA({
+			manifest: {
+				name: "IssaKass portfolio website",
+				short_name: "IssaKass",
+				description:
+					"Abdallah-Issa Kass's full stack developer portfolio website",
+				icons: [
+					{
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "/pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+				],
+			},
+			devOptions: {
+				enabled: true,
+			},
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,pdf}"],
+				runtimeCaching: [
+					{
+						urlPattern: ({ url }) => {
+							return url.pathname.startsWith("/api");
+						},
+						handler: "CacheFirst",
+						options: {
+							cacheName: "api-cache",
+							cacheableResponse: {
+								statuses: [0 - 200],
+							},
+						},
+					},
+				],
+			},
+		}),
+	],
+	build: {
+		outDir: "./dist/",
+		emptyOutDir: true,
+	},
 });
